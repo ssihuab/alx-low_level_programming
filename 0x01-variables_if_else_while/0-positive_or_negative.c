83abb3860fbbb4e3e9094d1362294941e036e12a
ssihuab <sihuabnuanteib@gmail.com>
2022-04-11 04:44:49 -0700

    to check the sign of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else (n > 0)
+	else if (n > 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
