cd083f64c22c619bee50e6ebf839509b78b9ba5e
ssihuab <sihuabnuanteib@gmail.com>
2022-04-11 04:18:02 -0700

    the second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf(n,"is negative");
+		printf("%d",n,"is negative");
 	else if (n == 0)
-		printf(n,"is zero");
+		printf("%d",n,"is zero");
 	else (n > 0)
-		printf(n,"is negative");
+		printf("%d",n,"is negative");
 	return (0);
 }
