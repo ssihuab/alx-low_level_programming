735905bbe58cd899217acd0accdce52c246d81ae
ssihuab <sihuabnuanteib@gmail.com>
2022-04-11 04:40:20 -0700

    to check the sign of a given number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - generates arandom number and check whether it is positive negative or  *        zero
+ *
+ * Return: Always 0 .
+ */
 int main(void)
 {
 	int n;
