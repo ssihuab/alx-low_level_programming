3113e38ceddbff0915688ff6166d69f996eb1ed2
ssihuab <sihuabnuanteib@gmail.com>
2022-04-12 08:48:15 -0700

    to print lower case alphabets back ward

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- * main - it prints Lowercase English Alphabets
+ * main - it prints Lowercase English Alphabets back ward
  *
  * Return: Always 0 .
  *
  */
 int main(void)
 {
-	char ch = 'a';
+	char ch = 'z';
 
-	while (ch <= 'z')
+	while (ch >= 'a')
 		{
 		putchar(ch);
-		ch++;
+		ch--;
 		}
 	putchar('\n');
 	return (0);
 }
+
