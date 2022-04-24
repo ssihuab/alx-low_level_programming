11d2bdca77bf10e6a7a70384182bee86422548a9
ssihuab <sihuabnuanteib@gmail.com>
2022-04-24 03:04:30 -0700

    this is to reset the value of n form 402 to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of n to 98
+ * @m: pointer variable
+ *
+ */
+void reset_to_98(int *m)
+{
+	*m = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a read me file for project 0x05-pointers_arrays_strings
